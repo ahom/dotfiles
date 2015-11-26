602edaf7b8dd9376b0c7c60d374c3388a6b8fbba
ahom <antoine.hom@gmail.com>
2015-11-26 22:17:50 +0100

    EDITOR

--- shell/fish.cfgsymlink/config.fish
+++ shell/fish.cfgsymlink/config.fish
@@ -1,7 +1,5 @@
 #! /usr/bin/fish
 
-abbr -a vim nvim
-
 abbr -a gs git status -b -s
 abbr -a gc git commit -m "
 abbr -a gca git commit -a -m "
@@ -9,5 +7,8 @@ abbr -a gco git checkout
 
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
 alias chrome="google-chrome-stable"
+alias vim="nvim"
+alias vi="nvim"
 
+set -x EDITOR nvim
 set -x DOTFILES $HOME/.dotfiles
