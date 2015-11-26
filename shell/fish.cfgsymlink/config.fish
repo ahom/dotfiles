a0255103d31d28fa385a26bb3aecadc42ff612ac
ahom <antoine.hom@gmail.com>
2015-11-26 22:14:45 +0100

    abbr update

--- shell/fish.cfgsymlink/config.fish
+++ shell/fish.cfgsymlink/config.fish
@@ -3,8 +3,8 @@
 abbr -a vim nvim
 
 abbr -a gs git status -b -s
-abbr -a gc git commit
-abbr -a gca git commit -a
+abbr -a gc git commit -m "
+abbr -a gca git commit -a -m "
 abbr -a gco git checkout
 
 alias glog="git log --graph --pretty=format:'%Cred%h%Creset %an: %s - %Creset %C(yellow)%d%Creset %Cgreen(%cr)%Creset' --abbrev-commit --date=relative"
