ee5c1062626bf2c392d8c766f616b2a393945795
ahom <antoine.hom@gmail.com>
2017-02-19 21:12:53 +0100

    neovim to vim and screenshot command

--- editor/install.sh
+++ editor/install.sh
@@ -1,7 +1,5 @@
 #! /bin/sh
 
-sudo pacman -Sy --noconfirm neovim python2 xsel
-sudo pip2 install neovim
-sudo pip install neovim
+sudo pacman -Sy --noconfirm vim xsel
 
-curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
